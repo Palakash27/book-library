d31b5a5e6a849c3bcc6b2d3671cd054159b3d7d3
Aakash Pal <Aakash@cloudthing.com>
2020-02-07 13:28:49 +0530

    Updates

---
+++ precache-manifest.e0cd3d475bd92cac48bd0fdd5a632a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318d1590d03b487b78efef668800de48",
+    "url": "/book-library/index.html"
+  },
+  {
+    "revision": "08f41475dc2bdcd95d66",
+    "url": "/book-library/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4b3ef96af426dc300983",
+    "url": "/book-library/static/js/2.a2463836.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/book-library/static/js/2.a2463836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f41475dc2bdcd95d66",
+    "url": "/book-library/static/js/main.336808b2.chunk.js"
+  },
+  {
+    "revision": "8b1ff4bf7d025e7f9508",
+    "url": "/book-library/static/js/runtime-main.ddfa8aa4.js"
+  }
+]);
